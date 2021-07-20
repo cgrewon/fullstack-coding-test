f0ae32c5b23abbcdb623fd8d2446fc871b5d5ce4
cgrewon <zhengyunxm@gmail.com>
2021-07-20 14:03:48 +0800

    Test1-finished(Extra too)

--- components/DynamicText.tsx
+++ components/DynamicText.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
+import { Input, Box, Flex } from "@chakra-ui/react"
 
-const DynamicText = () => {
+const DynamicText = React.forwardRef((props, ref) => {
+  
   const [value, setValue] = useState("Random Text");
 
-  const changeValue = (newValue) => {
-    setValue(newValue);
-  };
-
-  return <h1>{value}</h1>;
-};
+  React.useImperativeHandle(ref, () => ({
+    changeValue(newValue) {
+      setValue(newValue);
+    }
+  }))
+  
+  return <Box
+    textStyle="h1"
+    p={2}
+    maxW={{base:"18em",  sm:"28em", md:"48em", lg:"62em", xl:"80em"}}
+  >
+    {value}
+  </Box>;
+});
 
 export default DynamicText;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,57 @@
+import React from "react"
+
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import DynamicText from "../components/DynamicText";
+import DynamicText from "components/DynamicText";
+import { Input, Box, Flex } from "@chakra-ui/react"
+
+
 
 const Home = () => {
+
+  const ref = React.useRef();
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
+    let val = e.target.value;    
+    ref?.current?.changeValue(val);    
   };
+ 
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Coding Test</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Flex
+        minH="100vh"
+        p={"0 0.5rem"}
+        direction="column"
+        justify="center"
+        align="center"        
+      >
 
-      <main className={styles.main}>
-        <DynamicText />
-        <input onChange={onChange} />
-      </main>
-    </div>
+        <Head>
+          <title>Coding Test</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Flex
+          flex={1}
+          p="5rem 0"
+          direction="column"
+          justify="center"
+          align="center"
+        >
+
+        <DynamicText ref={ref}/>
+          
+        <Input
+          placeholder="large size"
+          size="lg"
+          onChange={onChange}
+          mt={6}
+        />
+
+        </Flex>
+      </Flex>
+    // </div>
   );
 };
 
